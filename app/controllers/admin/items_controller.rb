13b3efa6d8c9e9622588c986e9ca00a4373d412e
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-16 11:41:22 +0000

    admin item update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,24 +1,43 @@
 class Admin::ItemsController < ApplicationController
   
   def new
+    @item = Item.new
   end
 
   def index
+    @items = Item.all
   end
   
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_show_path
+    
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
   
   def update
+    @item = Item.find(paramas[:id])
+    @item.update(item_params)
+    redirect_to admin_items_show_path
   end
   
   def destroy
+    @item = Item.find(paramas[:id])
+    @item.destroy
+    redirect_to admin_items_index_path
   end
   
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :price, :is_active, :image)
+  end
 end
