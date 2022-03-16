52500d0d3646a0eebcaa10f09c488c691943cfd3
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-16 05:51:42 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,45 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/complete'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsbscribe'
+  end
+  devise_for :customers
+  devise_for :admins
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
