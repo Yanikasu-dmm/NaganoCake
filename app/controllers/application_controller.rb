a7bf89a6b43e80ee8ec29077f3d5fa0b90ebcc11
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-17 13:16:57 +0000

    [Update]DeviseCustomer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_cart
-  
+
   def current_cart
     if current_customer
       # ユーザーとカートの紐付け
@@ -14,4 +14,11 @@ class ApplicationController < ActionController::Base
     end
     current_cart
   end
+
+  private
+
+  # def configrure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
+  # end
 end
--- db/migrate/20220316053503_devise_create_customers.rb
+++ db/migrate/20220316053503_devise_create_customers.rb
@@ -6,14 +6,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
        t.datetime :remember_created_at
       t.string :last_name,   null: false, default: ""
-      t.string :first_name,  null: false, defalt: ""
-      t.string :last_name_kana,   null: false, defalt: ""
-      t.string :first_name_kana,  null: false, defalt: ""
-      t.string :address,    null: false, defalt: ""
-      t.string :post_code,  null:false, defalt: ""
+      t.string :first_name,  null: false, default: ""
+      t.string :last_name_kana,   null: false, default: ""
+      t.string :first_name_kana,  null: false, default: ""
+      t.string :address,    null: false, default: ""
+      t.string :post_code,  null: false, default: ""
       t.string :email,        null: false, default: ""
-      t.string :phone_number,    null: false, defalt: ""
-      t.boolean :is_deleted,  null:false, defalt: "FALSE"
+      t.string :phone_number,    null: false, default: ""
+      t.boolean :is_deleted,  null:false, default: "FALSE"
 
       t.string :encrypted_password, null: false, default: ""
 
