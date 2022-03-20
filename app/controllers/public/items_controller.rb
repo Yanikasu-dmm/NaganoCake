213c51389f8bf35003541e16c8d6c3b48636f68c
EC2 Default User <ec2-user@ip-172-31-5-25.ec2.internal>
2022-03-20 08:26:33 +0000

    [add]layoutの調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem
   end
