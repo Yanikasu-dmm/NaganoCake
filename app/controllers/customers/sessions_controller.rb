cecdae1a1ddd812fbc57368d9d2c9f8f10aecac4
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-21 12:00:33 +0000

    [Update]

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Customers::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :confirm_customer_is_deleted, only: [:create]
+
+
 
   # GET /resource/sign_in
   # def new
@@ -18,8 +20,15 @@ class Customers::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
+  def confirm_customer_is_deleted
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      redirect_to new_customer_session_path
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
