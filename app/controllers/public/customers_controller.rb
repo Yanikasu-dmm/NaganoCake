5201505b96df1efd84b8c977a2dda8ca583da0d7
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-21 05:17:02 +0000

    [Add]Tailwind

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,8 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def unsbscribe
