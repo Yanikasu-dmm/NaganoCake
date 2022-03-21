98fd6b59f193c4c0770f8f4936b3d1b09b879778
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-21 07:30:16 +0000

    Revert "Update"
    This reverts commit 95c958bf7856d638ed642ffb00ae8fb0b81bda77.

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-
+    
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
       @items = @genre.items.all
