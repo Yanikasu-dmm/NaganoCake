b3793136a410d955a4b7836455eca7feef9fb9a5
EC2 Default User <ec2-user@ip-172-31-36-14.ec2.internal>
2022-03-22 06:08:05 +0000

    validates

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  
-  validates :adress_code, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
+
+  validates :amount, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
   def sum_of_price
     item.get_tax_include_price * amount
   end
